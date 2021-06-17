82a39669a2c865ef80f3238ec9c972b40565b458
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-06-17 09:14:24 -0300

    Refacts site controller and folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -10,6 +13,6 @@ Rails.application.routes.draw do
   devise_for :admins
   get 'welcome/index'
 
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
