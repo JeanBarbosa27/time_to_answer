f35ccaa4c1e9330d2391421e39b436a7109bf49d
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-07-21 08:56:57 -0300

    Implement SB Admin template into admins backoffice
    At this moment I didn't followed the same instructions at the course, because the teacher installed bootstrap and jquery libs through yarn and reported some compatibility troubles, then I considered that the course was created some time ago and I would probably have other issues that I don't want to workaround. So I decided to incorporate some necessary files and include them into assets pipeline flow, which worked well for me

---
+++ app/assets/javascripts/admins_backoffice/vendors.js
@@ -0,0 +1,4 @@
+//= require jquery.min
+//= require jquery.easing.min
+//= require bootstrap.bundle.min
+//= require sb-admin-2.min
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,14 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# CSS Precompiling
+Rails.application.config.assets.precompile += %w[
+  admins_backoffice.scss
+  admins_backoffice/vendors.css
+]
+
+# JS Precompiling
+Rails.application.config.assets.precompile += %w[
+  admins_backoffice/vendors.js
+]
