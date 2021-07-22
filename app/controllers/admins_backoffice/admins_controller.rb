15896d32a72ffe1ba8d32f05a14bba490f98acef
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-07-22 12:55:29 -0300

    Improves admins backoffice edit form

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,10 +1,33 @@
-class AdminsBackoffice::AdminsController < AdminsBackofficeController
-  def index
-    @admins = Admin.all
-  end
+module AdminsBackoffice
+  class AdminsController < AdminsBackofficeController
+    before_action :admin_params, only: %i[update]
 
-  def edit
-    @admin = Admin.find(params[:id])
+    def index
+      @admins = Admin.all
+    end
+
+    def edit
+      @admin = Admin.find(params[:id])
+    end
+
+    def update
+      admin = Admin.find(params[:id])
+
+      if admin.update(admin_params)
+        redirect_to admins_backoffice_admins_url, notice: I18n.t('messages.sucess.update.admin')
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def admin_params
+      params.require(:admin).permit(
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
   end
-  
 end
