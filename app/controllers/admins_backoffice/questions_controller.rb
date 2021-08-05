0fccd2adc498e5baecec4e3491b2669facacd8f7
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-08-05 13:13:43 -0300

    Questions routes and controller

---
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -0,0 +1,54 @@
+module AdminsBackoffice
+  class QuestionsController < AdminsBackofficeController
+    before_action :set_question, only: %i[edit update destroy]
+
+    def index
+      @questions = Question.order(:description).page params[:page]
+    end
+
+    def new
+      @question = Question.new
+    end
+
+    def create
+      @question = Question.new(question_params)
+
+      if @question.save
+        redirect_to admins_backoffice_questions_url,
+                    notice: "#{@question.description} #{I18n.t('messages.success.create')}"
+      else
+        render :edit
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @question.update(question_params)
+        redirect_to admins_backoffice_questions_url,
+                    notice: "#{@question.description} #{I18n.t('messages.success.update')}"
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      if @question.destroy
+        redirect_to admins_backoffice_questions_url,
+                    notice: "#{@question.description} #{I18n.t('messages.success.delete')}"
+      else
+        render :index
+      end
+    end
+
+    private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:subject).permit(:description)
+    end
+  end
+end
