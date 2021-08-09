5d63415b4f7579070d618dbc3065d71cb312fb98
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-08-09 08:30:26 -0300

    Making search functional

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,9 @@
 module Site
   class SearchController < SiteController
     def questions
-      @questions = Question.includes(:answers).page(params[:page])
+      @questions = Question.includes(:answers)
+                           .where('lower(description) LIKE ?', "%#{params[:term].downcase}%")
+                           .page(params[:page])
     end
   end
 end
