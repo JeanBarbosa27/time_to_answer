c76cd28c66ede3d8fb5dccac60c2ecaf0f10253e
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-08-12 09:50:24 -0300

    Skip user name validation when reseting password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_global_params
   before_action :restrict_pagination
 
   layout :layout_by_resource
@@ -13,5 +14,8 @@ class ApplicationController < ActionController::Base
   def restrict_pagination
     params.extract!(:page) unless user_signed_in? || admin_signed_in?
   end
-  
+
+  def set_global_params
+    $global_params = params
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, presence: true, length: { minimum: 3 }, on: :update
+  validates :first_name, :last_name,  presence: true, length: { minimum: 3 },
+                                      on: :update, unless: :reset_password_token_present?
 
   has_one :user_profile
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
@@ -21,4 +22,7 @@ class User < ApplicationRecord
     AdminStatistic.increment_by_event(AdminStatistic::EVENTS[:total_users])
   end
   
+  def reset_password_token_present?
+    !!$global_params[:user][:reset_password_token]
+  end
 end
